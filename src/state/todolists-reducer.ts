0451b39c6c6a6e3e3f0f2e476565f49bdbb979ad
PezoDen <pezo1981@gmail.com>
2021-05-05 18:19:57 +0300

    tests tudo

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,17 +1,42 @@
 import {TodolistType} from "../App";
+import {v1} from "uuid";
+import {idID} from "@material-ui/core/locale";
 
 type ActionType = {
     type: string
     [key: string]: any
 }
 
-export const todolistsReducer = (state: Array<TodolistType>, action: ActionType):Array<TodolistType> => {
+export const todolistsReducer = (state: Array<TodolistType>, action: ActionType): Array<TodolistType> => {
     switch (action.type) {
         case 'REMOVE-TODOLIST': {
             return state.filter(tl => tl.id != action.id)
         }
-
+        case 'ADD-TODOLIST': {
+            return [
+                ...state,
+                {
+                    id: v1(),
+                    title: action.title,
+                    filter: 'all'
+                }
+            ]
+        }
+        case 'CHANGE-TODOLIST-TITLE' : {
+            const todolist = state.find(tl => tl.id === action.id)
+            if (todolist) {
+                todolist.title = action.title
+            }
+            return [...state]
+        }
+        case 'CHANGE-TODOLIST-FILTER':
+            let todolist = state.find(tl => tl.id === action.id)
+            if (todolist) {
+                todolist.filter = action.filter
+            }
+            return [...state]
         default:
             throw new Error("I don't understand this type")
     }
 }
+
